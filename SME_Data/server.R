df6009d25129ea0ad0b8b9dc36e30dec4ceafd6b
Gerard <ggallagher21@qub.ac.uk>
2022-02-21 22:27:26 +0000

    Updating Files

--- SME_Data/server.R
+++ SME_Data/server.R
@@ -8,30 +8,38 @@ package_list <-
     "ggplot2",
     "ggthemes",
     "haven",
-    "openxlsx",    
+    "openxlsx",
     "readxl",
     "plotly",
     "seasonalview",
     "shiny",
-    "TTR",  
+    "TTR",
     "zoo"
   )
 
+library_packages_for_app <- function(package_list = package_list)
+{
+  for (package in package_list) {
+    library(package, character.only = TRUE)
+  }
+}
 library_packages_for_app(package_list)
+
 
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    })
-
+  output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x,
+         breaks = bins,
+         col = 'darkgray',
+         border = 'white')
+    
+  })
+  
 })
